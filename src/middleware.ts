4fde0e94c801b3e4e4e71e880073916a7c3d033a
Robilol <robin.regis78@gmail.com>
2024-10-03 18:08:03 +0200

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import { updateSession } from './utils/middleware'
+import { updateSession } from '../utils/middleware'
 
 const blockedRoutes = ['/dashboard', '/cheese', '/auth', '/account']
 
